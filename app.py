71c41e423700c9c789b383a481523351c14ec6fc
Robert Mitchem <robert.mitchem@mari.com>
2026-03-29 02:03:34 -0400

    added initial http call to weather api using envvars and params

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
+import requests
 from flask import Flask
+
+from constants import WEATHER_API_KEY
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+@app.route('/weather/<station_id>', methods=['GET'])
+def weather(station_id='KMAHANOV10'):
+    print(f'Station: {station_id}')
+
+    REQUEST_URL = f'https://api.weather.com/v2/pws/observations/current?stationId={station_id}&format=json&units=e&apiKey={WEATHER_API_KEY}'
+    response = requests.get(REQUEST_URL)
+    if response.ok:
+        payload = response.json()
+        observation = payload['observations'][0]
+        return {
+            'heatIndex': f"{observation['imperial']['heatIndex']}f",
+            'windChill': f"{observation['imperial']['windChill']}f",
+            'temp': f"{observation['imperial']['temp']}f",
+            'humidity': observation['humidity'],
+            "obsTimeLocal": observation['obsTimeLocal'],
+            "stationID": observation['stationID']
+        }
+    else:
+        print(response.content)
 
 
 if __name__ == '__main__':
---
+++ constants.py
@@ -0,0 +1,3 @@
+import os
+
+WEATHER_API_KEY = os.environ.get('WEATHER_API_KEY')
